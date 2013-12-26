78f197c9585d0fdbee5f4d0c0f5ad3dbc3b756dc
andrey <ka@upsale.ru>
2013-12-26 08:29:04 +0200

    Solved Most Wanted Letter

--- level2/most_wanted_letter.py
+++ level2/most_wanted_letter.py
@@ -10,15 +10,21 @@ Input: A text for analysis. A string (Unicode).
 
 Output: The most frequent letter. A string.
 """
+from collections import Counter
+import re
 
 
 def checkio(text):
-
-    #replace this for solution
-    return 'a'
+    p = re.compile('\W+?')
+    clear_text = p.sub('', text.lower())
+    c = Counter(clear_text)
+    max_count = max(c.values())
+    max_letters = [k for k, i in c.items() if i == max_count]
+    max_letters.sort()
+    return max_letters[0]
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("Hello?,..,.-- World!") == "l", "Hello test"
     assert checkio("How do you do?") == "o", "O is most wanted"
     assert checkio("One") == "e", "All letter only once."
